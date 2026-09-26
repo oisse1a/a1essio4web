31a5ce50a5a3693769f104a870d07e9f72649aa3
来笑一个吧 <a1essio@qq.com>
2026-09-26 17:27:54 +0800

    feat:style init

--- apps/portal/src/components/layout/footer.tsx
+++ apps/portal/src/components/layout/footer.tsx
@@ -4,9 +4,9 @@ export default function Footer() {
   const year = new Date().getFullYear();
 
   return (
-    <footer className="mt-20 w-full flex justify-center fixed bottom-0 border-t border-(--line) px-4 pb-14 pt-10 text-(--sea-ink-soft)">
-      <div className="page-wrap flex flex-col items-center justify-between gap-4 text-center sm:flex-row sm:text-left">
-        <p className="m-0 text-sm">
+    <footer className="w-full max-w-270 mx-auto flex justify-center border-t border-(--line) px-4 pb-14 pt-10 text-(--sea-ink-soft)">
+      <div className="page-wrap text-sm flex flex-col items-center justify-between gap-4 text-center sm:flex-row sm:text-left">
+        <p className="m-0">
           &copy; {year} {m.calm_green_otter()}. All rights reserved.
         </p>
         <p className="island-kicker m-0">Built with TanStack Start & CloudBase</p>
--- apps/portal/src/components/layout/header.tsx
+++ apps/portal/src/components/layout/header.tsx
@@ -1,20 +1,17 @@
 import { Link } from "@tanstack/react-router";
 
-import ThemeToggle from "../ThemeToggle";
+import ThemeToggle from "../theme-toggle";
 import LangToggle from "../lang-toggle";
 import { m } from "~/app/paraglide/messages";
-import { Button } from "@repo/shadcn/components/ui/button";
 
 export default function Header() {
   return (
-    <header className="sticky top-0 z-50 border-b border-(--line)  px-4 ">
-      <nav className="page-wrap flex flex-wrap items-center gap-x-3 gap-y-2 py-3 sm:py-4">
-        <Button variant="outline" className="m-0 shrink-0 text-base font-semibold tracking-tight">
-          <Link to="/">{m.calm_green_otter()}</Link>
-        </Button>
+    <header className="w-full max-w-270 mx-auto sticky top-0 z-50 border-b border-(--line)  px-4 ">
+      <nav className="flex flex-wrap items-center gap-x-3 gap-y-2 py-3 sm:py-4">
+        <Link to="/">{m.calm_green_otter()}</Link>
 
         <div className="order-3 flex w-full flex-wrap items-center gap-x-4 gap-y-1 pb-1 text-sm font-semibold sm:order-0 sm:w-auto sm:flex-nowrap sm:pb-0">
-          <Link to="/" className="nav-link" activeProps={{ className: "nav-link is-active" }}>
+          <Link to="/" className="">
             {m.quiet_silver_bird()}
           </Link>
         </div>
